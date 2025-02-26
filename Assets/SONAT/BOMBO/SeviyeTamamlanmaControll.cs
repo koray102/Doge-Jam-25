b11e6543542a12e23fcdd4901aae619b9badd11c
GTOfan29 <>
2025-02-26 15:30:18 +0300

    koraya giren commit

--- Assets/SONAT/BOMBO/SeviyeTamamlanmaControll.cs
+++ Assets/SONAT/BOMBO/SeviyeTamamlanmaControll.cs
@@ -20,54 +20,5 @@ public class SeviyeTamamlanmaControll : MonoBehaviour
 
     }
 
-    public bool SeviyeBittiMi()
-    {
-        string npcLayerName = "NPC";
-        // NPC layer'ýnýn indeksini alýn
-        int npcLayer = LayerMask.NameToLayer(npcLayerName);
-        // Hierarchy'deki tüm GameObject'leri alýn
-        GameObject[] allObjects = FindObjectsOfType<GameObject>();
-
-        // Parent objeleri tutmak için bir liste oluþturun
-        List<GameObject> parentObjects = new List<GameObject>();
-
-        foreach (GameObject obj in allObjects)
-        {
-            // Objeyi ve layer'ýný kontrol edin
-            if (obj.layer == npcLayer && obj.transform.parent == null)
-            {
-                // Eðer obje NPC layer'ýnda ve parent'ý yoksa, listeye ekleyin
-                parentObjects.Add(obj);
-            }
-        }
-        Debug.Log(parentObjects.Count);
-        
-        if (parentObjects.Count == 1 || parentObjects.Count < 1)
-        {
-
-            SeviyeBittiEffectleriniAc();
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-        
-
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(SeviyeGoreviBitti && collision.CompareTag("Player"))
-        {
-            gameManagerScript.SonrakiSeviye();
-        }
-    }
-
-    public void SeviyeBittiEffectleriniAc()
-    {
-        SeviyeGoreviBitti = true;
-        duman.Play();
-    }
-
 
 }
